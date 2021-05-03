c94e1715da967fe10f11c19e70dca866bbc35a03
ottenok-menya <vbakq1@gmail.com>
2021-05-03 20:57:32 +0600

    new line

--- test.py
+++ test.py
@@ -7,4 +7,12 @@ print('Shalom')
 # что показывает какие файлы и папки мы собираемся запушить
 # git commit -m "initial commit" - создает новый коммит с сообщением
 # git remote add origin <url> - связывает ваш проект с репозиторием
-# git remote -v - для того чтобы проверить предыдущую команду и вообще
+# git remote -v - для того чтобы проверить предыдущую команду и вообще с каким репозиторием
+# git push origin master - пушим коммит в репозиторий
+
+lst = ['Nurkyz', 'Adliet', 'Abai', 'Siymyk', 'Ulan', 'Adilet', 'Richard', 'Insan', 'Azim']
+for i in lst:
+    print(i[0])
+
+
+
