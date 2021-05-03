a5d6317b2c501bcc310143591f374b3e4bd5aae9
ottenok-menya <vbakq1@gmail.com>
2021-05-03 20:32:23 +0600

    added forms

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 print('salam')
 print('aleikum salam')
 print('Shalom')
+print('forms')
 # git init - команда инициализирующая гит.... создает также папку .гит
 # git add . - команда добавит все файлы в данной директории
 # git status - необязательная команда, но полезная тем,
@@ -8,3 +9,7 @@ print('Shalom')
 # git commit -m "initial commit" - создает новый коммит с сообщением
 # git remote add origin <url> - связывает ваш проект с репозиторием
 # git remote -v - для того чтобы проверить предыдущую команду и вообще
+# git push origin master - пушим коммит в репозитории
+# git checkout -m <branch_name> для перехода на новую ветку
+# git branch - проверка текущей ветки
+
